bc59b5a6bd55aa79a30981ec11674b6f653b5911
AlanWeik <alaan.m@hotmail.com>
2020-10-07 16:07:40 +0200

    Disaster

--- Project Parse xUnitTest/UnitTest1.cs
+++ Project Parse xUnitTest/UnitTest1.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
-
+using Xunit.Sdk;
 
 namespace Project_Parse_xUnitTest
 {
@@ -10,20 +12,32 @@ namespace Project_Parse_xUnitTest
         [Fact]
         public void ConvertsOrNo()
         {
+            {
 
-            //Arrange
-            string PhoneNumber = "zero, seven, nine, three, four, eight, one, six, six, three";
-            int actual = 0;
+                var one = new Dictionary <double, string>();
+                one.Add(1, "one");
+                Console.WriteLine(one[1]);
 
-            //Act
-            bool isParsable = Int32.TryParse(PhoneNumber, out actual);
-
-            //Assert
-            if (isParsable)
-                Console.WriteLine(actual);
-            else
-                Console.WriteLine("Could not be parsed");
-
+            }
         }
     }
 }
+
+    /*
+    //Arrange
+    string PhoneNumber = "zero, seven, nine, three, four, eight, one, six, six, three";
+    int actual = 0;
+
+    //Act
+    bool isParsable = double.Parse(PhoneNumber, out actual);
+
+    //Assert
+    if (isParsable)
+        Console.WriteLine(actual);
+    else
+        Console.WriteLine("Could not be parsed");
+    */
+
+
+    
+
--- Projekt 2/Program.cs
+++ Projekt 2/Program.cs
@@ -1,20 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ProjektArbete
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string PhoneNumber = "zero, seven, nine, three, four, eight, one, six, six, three";
-            int actual = 0;
+            /*string hello = "twelve";
+            int twelve = 12;
+            Convert.ToChar(hello);
+            Convert.ToUInt64(hello);*/
 
-            bool isParsable = Int32.TryParse(PhoneNumber, out actual);
+            /*var one = new Dictionary <double, string>();
+            one.Add(1, "one");
+            Console.WriteLine(one[1]);*/
 
-            if (isParsable)
-                Console.WriteLine(actual);
-            else
-                Console.WriteLine("Could not be parsed");
+            /*var two = new Dictionary<double, string>();
+            two.Add(2, "two");
+            Console.WriteLine(two[2]);*/
+
+            /*var stringNumbers = new Dictionary<string, double>();
+            Console.Write("Type numbers (in letters): ");
+            string input = Console.ReadLine();
+            stringNumbers.Add("one", 1); // Adderar värde .Add() metod
+
+
+            if (input == "one")
+            {
+                Console.WriteLine(stringNumbers["one"]);
+            }
+            Console.ReadKey();*/
+
+
+            string input;
+            do
+            {
+
+                try
+                {
+                    var stringNumbers = new Dictionary<string, double>();
+                    Console.Write("Type numbers:  ");
+                    input = Console.ReadLine();
+                    stringNumbers.Add("one", 1); // Adderar värde .Add() metod
+                    if (input == "one")
+                    {
+                        Console.WriteLine(stringNumbers["one"]);
+                    }
+                    if (input != "one")
+                    {
+                        Console.WriteLine($"Fel inmatning! Skriv bara 'one' ");
+                    }
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+            }
+            while (input != "one");
+
         }
     }
 }
