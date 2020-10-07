2e7e6b877071aafb8887c1739d1f1732882f51f7
AlanWeik <alaan.m@hotmail.com>
2020-10-07 12:06:26 +0200

    Test finished?

--- Project Parse xUnitTest/UnitTest1.cs
+++ Project Parse xUnitTest/UnitTest1.cs
@@ -2,26 +2,27 @@ using System;
 using System.Globalization;
 using Xunit;
 
+
 namespace Project_Parse_xUnitTest
 {
-    public class UnitTest1
+    public class UnitTest1AW
     {
         [Fact]
         public void ConvertsOrNo()
         {
 
             //Arrange
-            string numbers = "zero, seven, nine, three, four, eight, one, six, six, three";
-            int actual = 0793481663;
+            string PhoneNumber = "zero, seven, nine, three, four, eight, one, six, six, three";
+            int actual = 0;
 
             //Act
-            Convert.ToDouble(numbers);
-            actual = new char();
+            bool isParsable = Int32.TryParse(PhoneNumber, out actual);
 
             //Assert
-            Assert.Equal(numbers, actual);
-
-
+            if (isParsable)
+                Console.WriteLine(actual);
+            else
+                Console.WriteLine("Could not be parsed");
 
         }
     }
--- Projekt 2/Program.cs
+++ Projekt 2/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace Projekt_2
+namespace ProjektArbete
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kol Khara");
+            string PhoneNumber = "zero, seven, nine, three, four, eight, one, six, six, three";
+            int actual = 0;
+
+            bool isParsable = Int32.TryParse(PhoneNumber, out actual);
+
+            if (isParsable)
+                Console.WriteLine(actual);
+            else
+                Console.WriteLine("Could not be parsed");
         }
     }
 }
