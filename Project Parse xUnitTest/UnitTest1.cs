b3f6bfed3082b94e78e5bdd33834205b9219b34c
AlanWeik <alaan.m@hotmail.com>
2020-10-12 14:19:00 +0200

    New test.

--- Project Parse xUnitTest/UnitTest1.cs
+++ Project Parse xUnitTest/UnitTest1.cs
@@ -7,42 +7,51 @@ using System.Security.Cryptography.X509Certificates;
 using Xunit;
 using Xunit.Sdk;
 using Xunit.Abstractions;
-
+using Microsoft.VisualBasic.CompilerServices;
+using System.Data.Common;
 
 namespace Project_Parse_xUnitTest
 {
     public class UnitTest1AW
     {
-        private readonly ITestOutputHelper output;
+        /*private readonly ITestOutputHelper output;
 
         public UnitTest1AW(ITestOutputHelper output)
         {
             this.output = output;
-        }
+        }*/
 
 
-        [Fact]
+        /*[Fact]
         public void ConvertsOrNo()
         {
             {
-                //string input = "one";
+                /*string input = "one";
                 var number = new Dictionary <string, double>();
                 number.Add("one", 1);
                 if (number.ContainsValue(1))
                 {
                     output.WriteLine("Contains 1");
                 }
-
-                /*if (input == "one")
-                {
-                    Assert.Equal(input, number); // OM Dictionary <string, double> blir det Assert.Equal(1, number["one"]);
-                }
-                else
-                {
-                    Console.WriteLine("Wrong type of input, write 'one' damnit.");
-                }*/
-
             }
+        }*/
+
+        [Fact]
+        public void EqualOrNot()
+        {
+            var number = new Dictionary<string, double>();
+            number.Add("one", 1);
+
+            double output = 1;
+
+            Assert.Equal(output, number["one"]);
+        }
+
+        [Fact]
+        public void Calculate()
+        {
+            
         }
     }
 }
+
