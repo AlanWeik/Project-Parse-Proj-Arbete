412e438bf9b9e20f1f2d7c4b51d9d32452862d1b
Marcus Backholm <marcus.backholm@live.se>
2020-10-12 15:25:26 +0200

    Parse metod skapad i TEST

--- XUnitTestMarcus/UnitTest1.cs
+++ XUnitTestMarcus/UnitTest1.cs
@@ -25,5 +25,38 @@ namespace XUnitTestMarcus
                 }
             } while (input != "one");  // Passed!!!!
         }
+        [Fact]
+        public void TestParseing() // Parseing mtod för att se om det går att parsa och om värdena överstämmer. 
+        {
+            var stringNumbers = new Dictionary<string, double>();
+            stringNumbers.Add("one", 1); // metod som adderar till Dictionary.
+            stringNumbers.Add("two", 2);
+            stringNumbers.Add("three", 3);
+            stringNumbers.Add("four", 4);
+            stringNumbers.Add("five", 5);
+            stringNumbers.Add("six", 6);
+            stringNumbers.Add("seven", 7);
+            stringNumbers.Add("eight", 8);
+            stringNumbers.Add("nine", 9);
+            string input = "";
+
+            switch (input) // Switch-sats för inmatning
+            {
+                case "one plus one":
+                    double result = double.Parse(input); // Parsar input.
+                    break;
+                case "one minus one":
+
+                    break;
+                case "one divided one":
+
+                    break;
+                case "one multiplie one":
+
+                    break;
+
+            }
+
+        }
     }
 }
